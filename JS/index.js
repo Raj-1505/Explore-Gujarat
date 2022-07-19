5dba310b81b231dd64f83fe8e1d9860381dd73fe
Raj-1505 <93474661+Raj-1505@users.noreply.github.com>
2022-07-19 23:05:55 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -5,37 +5,37 @@ function changePage(id){
             window.location="HTML/dwarka.html"
             break;
         case 'button2':
-            window.location="/HTML/jamnagar.html"
+            window.location="HTML/jamnagar.html"
             break;
         case 'button3':
-            window.location="/HTML/kutch.html"
+            window.location="HTML/kutch.html"
             break;
         case 'button4':
-            window.location="/HTML/rajkot.html"
+            window.location="HTML/rajkot.html"
             break;
         case 'button5':
-            window.location="/HTML/junagadh.html"
+            window.location="HTML/junagadh.html"
             break;
         case 'button6':
-            window.location="/HTML/porbandar.html"
+            window.location="HTML/porbandar.html"
             break;
         case 'button7':
-            window.location="/HTML/gandhinagar.html"
+            window.location="HTML/gandhinagar.html"
             break;
         case 'button8':
-            window.location="/HTML/ahmedabad.html"
+            window.location="HTML/ahmedabad.html"
             break;
         case 'button9':
-            window.location="/HTML/dang.html"
+            window.location="HTML/dang.html"
             break;
         case 'button10':
-            window.location="/HTML/narmada.html"
+            window.location="HTML/narmada.html"
             break;
         case 'button11':
-            window.location="/HTML/panchmahal.html"
+            window.location="HTML/panchmahal.html"
             break;
         case 'button12':
-            window.location="/HTML/surat.html"
+            window.location="HTML/surat.html"
             break;
         default:
             console.log("Oopps!! Somthing is Wrong!.")
